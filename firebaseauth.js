3303f474795782cc2e0f93dc0a584beebd9ab1b6
HemaTechno <ibrahimmansour4655@gmail.com>
2024-10-04 15:32:32 +0300

    Update firebaseauth.js

--- firebaseauth.js
+++ firebaseauth.js
@@ -6,6 +6,14 @@
  const firebaseConfig = {
  //YOUR COPIED FIREBASE PART SHOULD BE HERE
  //WATCH THIS VIDEO TO LEARN WHAT TO PUT HERE   https://youtu.be/_Xczf06n6x0
+apiKey: "AIzaSyDKEZYK77OPqDro7wEDm4iVssl2srSjC2g",
+  authDomain: "hrma-5ec84.firebaseapp.com",
+  projectId: "hrma-5ec84",
+  storageBucket: "hrma-5ec84.appspot.com",
+  messagingSenderId: "555454019427",
+  appId: "1:555454019427:web:2e014a372497051d9d4556"
+
+ 
  };
 
  // Initialize Firebase
